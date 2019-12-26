86f426422cb01665594a1c106d241574dee16d46
aldi <15065804+aldi@users.noreply.github.com>
2019-12-26 23:19:16 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,69 @@
+window.onload = function() {
+	var search_results = 20;
+	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+		search_results = 10;
+    }
+	var browserlang = navigator.language || navigator.userLanguage; 
+	document.getElementById("search_button").onclick = function() {
+		$("form").on("submit", function(event) {
+			event.preventDefault();
+      var city = $("#input").val(); //Get value from form input
+      location = "https://www.google.com/search?as_q=" + city;
+  });
+	};
+	$(function() {
+		function log(message) {
+			$("<div>")
+			.text(message)
+			.prependTo("#log");
+			$("#log").scrollTop(0);
+		}
+
+		$(window).on("resize", function() {
+			$("#ui-id-1").css("width", $("#input").width(), "important");
+		});
+
+		$("#input").autocomplete({
+			width: 780,
+			delay: 800,
+			source: function(request, response) {
+				$.ajax({
+					url:
+					"http://35.180.182.8/search?keywords=" +
+					$("#input").val() +
+					"&language=" + browserlang.substring(0, 2) + "&limit=" + search_results,
+					dataType: "json",
+					data: request,
+					success: function(data) {
+						response(
+							$.map(data.entries, function(value, key) {
+								return {
+									label: value.name
+								};
+							})
+							);
+					}
+				});
+			},
+			minLength: 2,
+			select: function(event, ui) {
+				if (ui.label) {
+					$('#search_button').attr('disabled',true);
+				}
+				else {
+					$('#search_button').attr('disabled',false);
+				}
+			},
+			open: function() {
+				$(this)
+				.removeClass("ui-corner-all")
+				.addClass("ui-corner-top");
+			},
+			close: function() {
+				$(this)
+				.removeClass("ui-corner-top")
+				.addClass("ui-corner-all");
+			}
+		});
+	});
+};
